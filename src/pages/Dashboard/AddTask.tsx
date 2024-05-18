d92af7dfe5c562afcc69f65c7ad2f5f88ddf4e64
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-19 02:41:03 +0300

    Lists context type issue

---
+++ src/pages/Dashboard/AddTask.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { useListsForForms } from '../../contexts/global/ListsForForms'
+
+const AddTask = () => {
+  const { allProjects, allUsers } = useListsForForms()
+
+  return <div></div>
+}
+
+export default AddTask
